6dd5e99345d8419960fe976766d5ccedcb97cd64
bruno-wolf <bruno.m.lobato@gmail.com>
2021-02-20 14:50:24 +0000

    configs server using express

---
+++ src/server.ts
@@ -0,0 +1,22 @@
+import express from 'express';
+import routes from './routes';
+
+const app = express();
+app.use(express.json());
+app.use(routes);
+
+app.listen(3333, () => {
+  console.log(`
+  ---------------------------------
+  
+  Wolf Server started on port 3333
+  🐺 🤟
+  
+  ---------------------------------
+  `)
+})
+
+
+
+
+
