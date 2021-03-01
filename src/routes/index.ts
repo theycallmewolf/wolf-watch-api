d8b87f2f21b18ec1a8c2b9d7a9a51807a3ba42ee
bruno-wolf <bruno.m.lobato@gmail.com>
2021-03-01 19:27:50 +0000

    adds WOEIDController

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,29 +1,14 @@
 import { Router } from "express";
-import getWeather from '../services/getWeather';
 import { CityController } from '../controllers/CityController';
+import { WOEIDController } from '../controllers/WOEIDController';
 
 const routes = Router();
 
 const cityController = new CityController();
+const woeIDController = new WOEIDController();
 
-routes.get('/weather/:woeid', async (request, response) => {
-  const { woeid } = request.params;
-  
-  const weatherForecast = await getWeather({
-    woeid,
-    date: new Date(),
-  });
-
-  if(!weatherForecast) { 
-    response.status(404).json({
-      type: 'error',
-      message : 'not found',
-    })
-  }
-
-  return response.status(200).json(weatherForecast.data[0]);
-});
-
+routes.get('/weather/:woeid', woeIDController.execute);
 routes.get('/location/:city', cityController.execute);
+
 
 export default routes;
