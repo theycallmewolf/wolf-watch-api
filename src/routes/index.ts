c1b39b2c07a6c9c581519155eb9b2e41af0dc27d
bruno-wolf <bruno.m.lobato@gmail.com>
2021-03-01 19:35:28 +0000

    renames routes to router

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,13 +2,13 @@ import { Router } from "express";
 import { CityController } from '../controllers/CityController';
 import { WOEIDController } from '../controllers/WOEIDController';
 
-const routes = Router();
+const router = Router();
 
 const cityController = new CityController();
 const woeIDController = new WOEIDController();
 
-routes.get('/weather/:woeid', woeIDController.execute);
-routes.get('/location/:city', cityController.execute);
+router.get('/weather/:woeid', woeIDController.execute);
+router.get('/location/:city', cityController.execute);
 
 
-export default routes;
+export default router;
--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,12 @@
+import 'reflect-metadata';
 import express from 'express';
 import cors from 'cors';
-import routes from './routes';
+import router from './routes';
 
 const app = express();
 app.use(cors());
 app.use(express.json());
-app.use(routes);
+app.use(router);
 
 app.listen(3333, () => {
   console.log(`
