30b7bbca669c25c8269ee0712651a62fcb2c3c42
bruno-wolf <bruno.m.lobato@gmail.com>
2021-03-01 19:42:35 +0000

    splits server.ts to app.ts

---
+++ src/app.ts
@@ -0,0 +1,25 @@
+import 'reflect-metadata';
+import express, { Request, Response, NextFunction } from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import router from './routes';
+import { AppError } from './errors/AppError';
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.use(router);
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  if(error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      message: error.message,
+    })
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: `Internal server error: ${error}`,
+  });
+});
+
+export { app };
